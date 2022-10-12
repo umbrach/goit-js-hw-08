c6d902afa5532d3ae162d78a8ffdd5db8c82560c
Denys Barabanov <umbrach415@gmail.com>
2022-10-12 21:18:50 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsArray = [];
+
+galleryItems.forEach(({ preview, original, description }) => {
+  galleryItemsArray.push(
+    `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  );
+});
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsArray.join(''));тзь
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

baa59f0d6a1a73cb3c8f3139f596220c1f005d31
Denys Barabanov <umbrach415@gmail.com>
2022-10-14 11:23:03 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0);
+
+const timeUpdate = data => {
+  console.log(data);
+  localStorage.setItem(LOCAL_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
